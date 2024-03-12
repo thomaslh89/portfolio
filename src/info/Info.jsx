a31f2f0d88f0aefc97e9898995176a4ff6edb9f6
Thomas <thomaslh89@gmail.com>
2024-03-12 21:44:08 +0100

    name commit

--- src/info/Info.jsx
+++ src/info/Info.jsx
@@ -22,9 +22,9 @@ of what each of the values mean.
  */
 
 export const info = {
-  firstName: "John",
-  lastName: "Smith",
-  initials: "js", // the example uses first and last, but feel free to use three or more if you like.
+  firstName: "Thomas",
+  lastName: "Le Hébel",
+  initials: "Tlh", // the example uses first and last, but feel free to use three or more if you like.
   position: "a Full Stack Developer",
   selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
   gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
