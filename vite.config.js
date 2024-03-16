19b8d2fd953d265a2f4a3390412da7e85fa278c8
Thomas <thomaslh89@gmail.com>
2024-03-16 11:23:32 +0100

    remove yarn favicon package

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import favicons from "@peterek/vite-plugin-favicons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), [favicons("src/assets/icon.svg")]],
+  plugins: [react()],
 });
