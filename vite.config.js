86967efe7f2dcbe1378d2af82a5b3cf15b3aade5
Thomas <thomaslh89@gmail.com>
2024-03-16 11:10:04 +0100

    add favicon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import favicons from "@peterek/vite-plugin-favicons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), [favicons("src/assets/icon.svg")]],
+});
